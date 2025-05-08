dbd73fa53ca636e75eef141d92bd856495fad857
RiddhiDeogade <deogaderiddhi@gmail.com>
2025-05-08 18:20:11 +0530

    ui modification committt

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	
-	
-	
 	@GetMapping("/")
 	public String home(Model m)
 	{
@@ -32,21 +29,17 @@ public class HomeController {
 		return "signin";
 	}
 
-
-
 	@GetMapping("/register")
 	public String register(Model m)
 	{
 		m.addAttribute("title","REGISTER");
 		return "register";
 	}
-	
-	
+
 	@GetMapping("/about")
 	public String about(Model m)
 	{
 		m.addAttribute("title","ABOUT");
 		return "about";
 	}
-
 }
